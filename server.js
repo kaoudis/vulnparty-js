231d9cbf2db9780701a848f1db51b47f717aeb2e
kaoudis <moshimoshi@kellykaoud.is>
2021-07-08 08:36:14 -0600

    switch to axios; adding tests next

--- server.js
+++ server.js
@@ -1,46 +1,36 @@
+const axios = require('axios');
 const express = require('express');
-const got = require('got');
 // we are specifically using a vulnerable version of private-ip
 const privateIp = require('private-ip');
 const url = require('url');
 
-// never do this - do not pass user input unsanitized
-// and this is awful code anyway.
+// do not pass user input unsanitized! this is only for demo purposes.
 async function getNext(nextLocation) {
-    try {
-        // janky but just to make this work for now (got likes a scheme)
-        const schemePrefix = 'http';
-        const requestUrl = !nextLocation.startsWith(schemePrefix) ? schemePrefix + '://' + nextLocation : nextLocation;
-        const response = await got(requestUrl);
-        console.log('response code: ' + response.statusCode);
-        console.log('response: ' + response.body);
-    } catch (err) {
-        console.log('UH OH: got back an error');
-        console.log(err);
-    }
+    await axios.get(nextLocation)
+        .then(response => console.log(response))
+        .catch(err => {
+            console.log('UH OH: request (to ' + nextLocation + ') failed: ');
+            console.log(err.message);
+        });
 }
 
 const get = (privado, request, response) => {
-    const loc = url.parse(request.url, true).query.nextRequest;
+    const queryParams = url.parse(request.url, true).query;
+    const loc = queryParams.nextRequest;
     // requires at least the parameter 'nextRequest' set to an IP address or similar
     console.log('next request will go to (raw): ' + loc);
 
-    const acceptable = privado ? privateIp(loc) : !privateIp(loc);
+    const acceptable = privado ? privateIp(loc) : !privateIp(loc); 
     const headers = {'Content-Type': 'text/html'};
 
     if (acceptable) {
         getNext(loc);
         response.writeHead(200, headers);
-        response.end('boom!');
+        response.end('attempted to hit ' + loc);
     } else {
         response.writeHead(403, headers);
         response.end('problemas: will not request ' + loc);
     }
-}
-
-const errs = (err, response) => {
-    console.log('errors: ');
-    console.log(err);
 }
 
 const app = express();
