9196eb22d58245c59c498e62433c4c1e8aebd228
kaoudis <moshimoshi@kellykaoud.is>
2021-07-08 08:39:27 -0600

    clean up console.logging a wee bit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const get = (privado, request, response) => {
     const queryParams = url.parse(request.url, true).query;
     const loc = queryParams.nextRequest;
     // requires at least the parameter 'nextRequest' set to an IP address or similar
-    console.log('next request will go to (raw): ' + loc);
+    console.log('attempting to request (raw): ' + loc);
 
     const acceptable = privado ? privateIp(loc) : !privateIp(loc); 
     const headers = {'Content-Type': 'text/html'};
@@ -28,6 +28,7 @@ const get = (privado, request, response) => {
         response.writeHead(200, headers);
         response.end('attempted to hit ' + loc);
     } else {
+        console.log('would not request ' + loc);
         response.writeHead(403, headers);
         response.end('problemas: will not request ' + loc);
     }
@@ -37,17 +38,19 @@ const app = express();
 const server = require('http').Server(app);
 const port = 8888;
 
-// no middleware for now I guess
+// no middleware for now I guess. 
+// could require some headers here or add Helmet or such.
 app.use((request, response, next) => {
     next();
 });
 
 app.get('/private', (request, response) => {
-    console.log('GET /private...');
+    console.log('GET /private');
     get(true, request, response);
 });
 
 app.get('/public', (request, response) => {
+    console.log('GET /public')
     get(false, request, response);
 });
 
