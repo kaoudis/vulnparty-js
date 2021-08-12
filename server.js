a58f4542b5139412a1d537586fa052bdcad6017c
kaoudis <moshimoshi@kellykaoud.is>
2021-08-11 23:03:17 -0600

    not entirely sure if this works but probably... can't run locally right now, will check from another machine. curse you unstable node version in arch

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const axios = require('axios').create({proxy: false});
 const curl = require('node-libcurl');
 const dns = require('dns');
 const express = require('express');
+const fs = require('fs');
 const ftp = require('ftp');
 // we are specifically using a vulnerable version of private-ip
 const privateIp = require('private-ip');
 const url = require('url');
 
 const { createLogger, format, transports, winston } = require('winston');
+const { response } = require('express');
 const { combine, timestamp, prettyPrint } = format;
 
 const headers = {'Content-Type': 'text/html'};
@@ -25,6 +27,7 @@ const logger = createLogger({
 		})
 	]
 });
+
 
 // do not pass user input unsanitized! this is only for demo purposes.
 // it's also kind of gross generally.
@@ -53,10 +56,10 @@ const get = (privado, request, response) => {
 	const loc = queryParams.nextRequest;
 	logger.debug('attempting to request (raw address passed by client): ' + loc);
 
-	if (loc !== null && loc !== '') {	
-		// just dump the entire unsanitized user input into privateIp()! this is not 
-		// ideally what would occur. private-ip after 1.0.5 will rightly only check 
-		// IP addresses and not an ip with scheme and/or port, but 1.0.5 will do 
+	if (loc !== null && loc !== '') {
+		// just dump the entire unsanitized user input into privateIp()! this is not
+		// ideally what would occur. private-ip after 1.0.5 will rightly only check
+		// IP addresses and not an ip with scheme and/or port, but 1.0.5 will do
 		// some fun stuff...
 		const acceptable = privado ? privateIp(loc) : !privateIp(loc);
 
@@ -73,22 +76,55 @@ const get = (privado, request, response) => {
 	} else {
 		logger.error('parameter \'nextRequest\' not passed; returning 400');
 		response.writeHead(400, headers);
-		response.end('we require \'nextRequest\' parameter on request');    
+		response.end('we require \'nextRequest\' parameter on request');
 	}
 }
 
-const ftpGet = (loc, fileName) => {
+const getBookByName = (bookName, response) => {
+	if (bookName.endsWith(".pdf")) {
+		// check for the file locally on the server
+		fs.readFile(bookName, (err, bookData) => {
+			if (err) {
+				logger.error(err);
+				response.writeHead(500, headers);
+				// never do this - there's no reason to pass back
+				// any kind of internal server error info to the client.
+				// not even useful information unless the client is a hacker.
+				response.end(`internal server error: ${err}`);
+			} else {
+				// never do this either - we have no idea what this file is
+				// we're just dumping it out bang to the client
+				response.writeHead(200, headers);
+				response.end(bookData);
+			}
+		});
+	} else {
+			let msg = `unacceptable file extension requested (as part of bookFileName '${bookName}')`;
+
+		logger.error(msg);
+		response.writeHead(400, headers);
+		response.end(msg);
+	}
+}
+
+const ftpGet = (loc, fileName, response) => {
 	const ftpClient = new ftp();
 	ftpClient.on('ready', () => {
 		ftpClient.get(fileName, (error, stream) => {
 			if (error) {
 				logger.error(error);
-				throw error;
+				// never do this - there's no reason
+				response.writeHead(500, headers);
+				response.end(`internal server error: ${error}`);
+			} else {
+				logger.debug('closing stream');
+				stream.once('close', () => { ftpClient.end(); });
+				stream.pipe((output) => {
+					logger.debug('got file!');
+					response.writeHead(200, headers);
+					response.end(output);
+				});
 			}
-
-			logger.debug('closing stream');
-			stream.once('close', () => { ftpClient.end(); });
-			stream.pipe((output) => { console.log(output); });
 		});
 	});
 
@@ -116,14 +152,14 @@ app.use((request, response, next) => {
 	next();
 });
 
-// if the potential nextRequest location is "private" according to 
+// if the potential nextRequest location is "private" according to
 // private-ip 1.0.5, request it
 app.get('/private', (request, response) => {
 	logger.debug('GET /private');
 	get(true, request, response);
 });
 
-// if the potential nextRequest location is "public" according to 
+// if the potential nextRequest location is "public" according to
 // private-ip 1.0.5, request it
 app.get('/public', (request, response) => {
 	logger.debug('GET /public')
@@ -133,27 +169,30 @@ app.get('/public', (request, response) => {
 // inspired by cors-anywhere
 app.get('/next/:nextRequest', (request, response) => {
 	logger.debug(`GET ${request.params.nextRequest}`);
-	getNext(request.params.nextRequest);   
+	getNext(request.params.nextRequest);
+});
+
+//retrieve a file! which could be a pdf!
+app.get('/library/books/:bookFileName', (request, response) => {
+	logger.debug(`GET ${request.params.bookFileName}`);
+	getBookByName(request.params.bookFileName, response);
 });
 
 // retrieve a remote file and output it to console
 app.get('/ftp', (request, response) => {
 	const queryParams = url.parse(request.url, true).query;
 	const loc = queryParams.nextRequest;
-	const fileName = queryParams.file;	
+	const fileName = queryParams.file;
 
-	// denylist sketch. the idea here is to show how a well-intentioned denylist may not end up 
+	// denylist sketch. the idea here is to show how a well-intentioned denylist may not end up
 	// providing that much security since there are plenty of ways to get around this.
 	if (!fileName.includes('localhost') && !loc.includes('localhost') && !loc.includes('127.0.0.1')) {
 		logger.debug(`GET ${fileName} from ftp://${loc}`);
-		ftpGet(loc, fileName);
-		// just empty 200 response for now
-		response.writeHead(200, headers);
-		response.end();
+		ftpGet(loc, fileName, response);
 	} else {
 		logger.error(`will not retrieve ${fileName} from ftp://${loc}`);
 		response.writeHead(400, headers);
-		response.end('did you include the `file` and `nextRequest` parameters?');    
+		response.end('did you include the `file` and `nextRequest` parameters?');
 	}
 });
 
